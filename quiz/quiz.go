a8963052580f4e60f6f53abb8fa32802e45c0fcc
Jonathon Pinnow <jonathon.pinnow@pursuanthealth.com>
2019-06-04 16:22:38 -0400

    add option to randomize questions. refactor answer compare into a function

--- quiz/quiz.go
+++ quiz/quiz.go
@@ -6,6 +6,7 @@ import (
 	"encoding/csv"
 	"flag"
 	"fmt"
+	"math/rand"
 	"os"
 	"strings"
 	"time"
@@ -21,13 +22,14 @@ func main() {
 
 	var seconds int
 	var filename string
+	var randomize bool
+
+	answeredCorrectly := 0
 
 	flag.IntVar(&seconds, "time-limit", 30, "sets the time limit of the quiz")
 	flag.StringVar(&filename, "path", "./problems.csv", "location of question and answer csv file")
+	flag.BoolVar(&randomize, "randomize", false, "Randomize the order of questions")
 	flag.Parse()
-
-	answeredCorrectly := 0
-	// numberOfQuestions := 0
 
 	// open file
 	f, err := os.Open(filename)
@@ -42,12 +44,19 @@ func main() {
 		panic(err)
 	}
 
+	// if randomize flag is set then shuffle the problems
+	if randomize {
+		lines = randomizeProblems(lines)
+	}
+
+	// initialize the prompt of the quiz and wait for user input to begin
 	reader := bufio.NewReader(os.Stdin)
 	fmt.Println("--- Quiz ---")
 	fmt.Println("------------")
 	fmt.Println("Press Enter to begin")
 	reader.ReadString('\n')
 
+	// setup and start the count down for the quiz
 	countDown := time.NewTimer(time.Duration(seconds) * time.Second)
 	go func() {
 		<-countDown.C
@@ -56,6 +65,7 @@ func main() {
 		os.Exit(0)
 	}()
 
+	// read through the various sets of questions and answers and check for correctness
 	for _, line := range lines {
 		data := Problem{
 			Question: line[0],
@@ -64,21 +74,37 @@ func main() {
 		fmt.Println("what is the answer to: " + data.Question + " ?")
 		answer, _ := reader.ReadString('\n')
 
-		if strings.Compare(cleanString(data.Answer), cleanString(answer)) == 0 {
-			answeredCorrectly++
-			fmt.Printf("Correct!\n\n")
-		} else {
-			fmt.Printf("Wrong :(\n-- correct answer: %s \n\n", data.Answer)
-		}
+		evaluateAnswer(&answeredCorrectly, data, answer)
 	}
 
 	result(answeredCorrectly, len(lines))
 }
 
+// result takes the number of correct answers and the total number of questions and outputs a message to the console
 func result(correct int, questions int) {
 	fmt.Printf("You answered %d out of %d correct", correct, questions)
 }
 
+// cleanString removes trailing and leading whitespace
 func cleanString(s string) string {
 	return strings.ToLower(strings.TrimSpace(s))
+}
+
+// randomizeProblems shuffles a slice of slices contianing question and answer strings
+func randomizeProblems(p [][]string) [][]string {
+	rand.Seed(time.Now().UnixNano())
+	rand.Shuffle(len(p), func(i int, j int) {
+		p[i], p[j] = p[j], p[i]
+	})
+	return p
+}
+
+// evaluateAnswer takes a problem type and will check a given answer and if it matches will increment a count pointer
+func evaluateAnswer(count *int, problem Problem, userAnswer string) {
+	if strings.Compare(cleanString(problem.Answer), cleanString(userAnswer)) == 0 {
+		*count++
+		fmt.Printf("Correct!\n\n")
+	} else {
+		fmt.Printf("Wrong :(\n-- correct answer: %s \n\n", problem.Answer)
+	}
 }
